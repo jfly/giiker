b167268047f379463382716ca8eff1b0d88ccf3f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-07-04 23:12:24 -0700

    Add a remote control toggle.

--- main.py
+++ main.py
@@ -2,17 +2,35 @@ from subprocess import call
 
 import giiker
 
+T_PERM = "B D B' D' B' L B B D' B' D' B D B' L'".split()
+TOGGLE_REMOTE = ("U U' "*2).split()
+
+remote_control = True
 move_history = []
 
-T_PERM = "B D B' D' B' L B B D' B' D' B D B' L'".split()
 
 def on_state_change(giiker_state):
+    global remote_control, move_history
+
     last_move = giiker_state.last_move
     move_history.append(last_move)
+
+    if alg_just_applied(TOGGLE_REMOTE):
+        remote_control = not(remote_control)
+        if remote_control:
+            print("Congratulations, you have entered remote control mode!")
+        else:
+            print("Happy cubing!")
+        move_history = []
+        return
+
+    if not remote_control:
+        return
 
     if alg_just_applied(T_PERM):
         print("Nice T perm!")
         call("xdotool key --clearmodifiers XF86AudioPlay", shell=True)
+        return
 
     if last_move.face == "U":
         if last_move.amount == 1:
@@ -20,6 +38,7 @@ def on_state_change(giiker_state):
         elif last_move.amount == -1:
             call("set_volume.py 5-", shell=True)
         call("show-volume.sh", shell=True)
+        return
 
 def alg_just_applied(alg):
     if len(move_history) < len(alg):
